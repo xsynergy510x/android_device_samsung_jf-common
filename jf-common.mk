285a7e7b6aa17e517b96d45b897c5cc71cb10b2d
dcd <dcd1182@gmail.com>
2014-11-23 01:46:19 -0600

    jf: Use Google's codecs
    Change-Id: I07df75370b694b510c08df9e0934afdd535d9813
    
    Conflicts:
    	jf-common.mk

--- jf-common.mk
+++ jf-common.mk
@@ -138,6 +138,13 @@ PRODUCT_PACKAGES += \
     loki_bootloaders \
     recovery-transform.sh
 
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Media Profile
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
