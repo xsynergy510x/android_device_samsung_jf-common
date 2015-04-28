2f34a28f76b27f2120347144581a2c04eec03675
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-28 15:32:01 -0500

    jf: switch to nuplayer, kill lpa
    Change-Id: I9785ccf5c8917826460ffd435de17f42a68e27f1

--- jf-common.mk
+++ jf-common.mk
@@ -231,7 +231,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # media
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.media.use-awesome=true \
     qcom.hw.aac.encoder=true
 
 # radio
@@ -255,7 +254,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # media
 PRODUCT_PROPERTY_OVERRIDES += \
-    lpa.decode=true \
     mm.enable.smoothstreaming=true
 
 # nitz
