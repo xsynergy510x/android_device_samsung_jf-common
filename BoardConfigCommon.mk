9694646e25e997d9b95a767469dd09a42c18bef7
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-15 11:23:12 -0500

    [DO NOT MERGE] jf-common: temp fixes to build M
    * selinux permissive
    * disable local sepolicies
    * disable non-existant overlays
    
    Change-Id: I760507bc4fd51ee4c7aabe9252bf64d133991f37

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ TARGET_CPU_VARIANT := krait
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -132,8 +132,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jf-common/releasetools
 BOARD_RIL_CLASS := ../../../device/samsung/jf-common/ril
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/samsung/jf-common/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += device/samsung/jf-common/sepolicy
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
