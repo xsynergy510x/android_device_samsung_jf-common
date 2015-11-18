dfaa188d0f8ab7c176bbafdbbd3534d864b34f34
Eduard Alfonso <xsynergy510x@gmail.com>
2015-11-17 20:16:14 -0800

    Revert "Add camera2 prop found in GPE"
    This reverts commit ffbff85521ff6f76a6c719e92ede15b4d5d8a0bf.

--- jf-common.mk
+++ jf-common.mk
@@ -239,10 +239,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.speaker.location=high \
     ro.qc.sdk.audio.fluencetype=fluence
 
-# camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
-
 # charging mode
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.enable_boot_charger_mode=1
